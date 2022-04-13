a4e2d983f910b3c86ae71b353ef914c8e6862ec4
qvzou <97709073+qvzou@users.noreply.github.com>
2022-04-13 03:28:45 +0000

    worked on pre01 book exercise and added pre02, worked on chapter 2 exercise

---
+++ pre02/Time.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Time {
+
+public static void main(String[] args){
+  int hour = 23;
+  int minute = 23;
+  int second = 03;
+  System.out.print("The time is ");
+  System.out.print(hour);
+  System.out.print(":");
+  System.out.print(minute);
+  System.out.print(":");
+  System.out.print(second);
+  System.out.println(".");
+  }
+}
