260f647c73dd6e39351eb48ecc3f133708c77303
qvzou <97709073+qvzou@users.noreply.github.com>
2022-04-22 00:15:46 +0000

    worked on prework 5 chapter 6 exercise 8, have stack overflow error

--- pre05/Methods.java
+++ pre05/Methods.java
@@ -7,6 +7,7 @@ public class Methods {
   public static void main(String[] args) {
         System.out.println(isDivisible(4, 1));
 System.out.println(isTriangle(30, 3, 5));
+        System.out.println(ack(2, 1));
     }
  
   public static boolean isDivisible(int m, int n) {
@@ -32,7 +33,23 @@ System.out.println(isTriangle(30, 3, 5));
 }
 
   /**
-  *Exercise 8 Write a method
+  *Exercise 8 Write a method that takes two ints as parameters and computes and returns the value of the 
+Ackermann function.
   */
-  
+
+  public static int ack(int n, int m) {
+    if (m == 0) {
+      return n + 1;
+    }
+    else if (m > 0 && n == 0) {
+      return ack(m - 1, 1);
+    }
+    else if (m > 0 && n > 0) {
+      return ack(m - 1, ack(m, n - 1));
+    }
+  return ack(m, n);
+    }
+  /**
+  *when m is greater than 0, stack overflow error occurs for 45 and 48.
+  */
 }
