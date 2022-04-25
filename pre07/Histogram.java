7c24ac87525494e87dc0a57f470a1a03c020c580
qvzou <97709073+qvzou@users.noreply.github.com>
2022-04-25 04:48:07 +0000

    prework 7

--- pre07/Histogram.java
+++ pre07/Histogram.java
@@ -6,13 +6,6 @@
     return counts;
   }
 
-  public static int[] randomArray(int size) {
-    
-    Random random = new Random();
-    int[] a = new int[size];
-    for (int i = 0; i < a.length; i++) {
-      a[i] = random.nextInt(100);
-    }
-    return a;
+
   }
 
